5e64e8b5f5f26e9e98c5b261b8fb6cfb317e97cb
achiyat <111647186+achiyat@users.noreply.github.com>
2023-09-21 19:29:19 +0300

    commit - I finished building Backend

---
+++ src/Backend/index.js
@@ -0,0 +1,29 @@
+// import dotenv from "dotenv";
+// import "dotenv/config";
+
+// const apiKey111 = process.env.API_KEY;
+
+const apiKey = "39525914-9152bde184a13a887fbc35f2b"; // Replace with your API key
+
+export const buildApiUrl = async (Word, perPage) => {
+  const baseUrl = "https://pixabay.com/api/";
+  const queryParams = {
+    key: apiKey,
+    q: Word, // Decode the formatted query
+    image_type: "photo",
+    lang: "en",
+    per_page: perPage,
+  };
+
+  // Construct the query string
+  const queryString = Object.keys(queryParams)
+    .map((key) => `${key}=${encodeURIComponent(queryParams[key])}`)
+    .join("&");
+
+  const query = queryString.replace("%2B", "+");
+
+  //   console.log(queryString);
+  //   console.log(queryString1);
+  //   console.log(`${baseUrl}?${query}`);
+  return `${baseUrl}?${query}`;
+};
--- src/components/SearchPhotos/searchPhotos.jsx
+++ src/components/SearchPhotos/searchPhotos.jsx
@@ -1,34 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getPhotoFromAPI } from "../../services/services";
+import { buildApiUrl } from "../../Backend";
 
 export const SearchPhotos = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [images, setImages] = useState([]);
   const [perPage, setPerPage] = useState(10);
-
-  const buildApiUrl = async (Word, perPage) => {
-    const apiKey = "39525914-9152bde184a13a887fbc35f2b";
-    const baseUrl = "https://pixabay.com/api/";
-    const queryParams = {
-      key: apiKey,
-      q: Word, // Decode the formatted query
-      image_type: "photo",
-      lang: "en",
-      per_page: perPage,
-    };
-
-    // Construct the query string
-    const queryString = Object.keys(queryParams)
-      .map((key) => `${key}=${encodeURIComponent(queryParams[key])}`)
-      .join("&");
-
-    const query = queryString.replace("%20", "+");
-
-    console.log(query);
-    //   console.log(queryString1);
-    //   console.log(`${baseUrl}?${query}`);
-    return `${baseUrl}?${query}`;
-  };
 
   const fetchImages = async () => {
     try {
